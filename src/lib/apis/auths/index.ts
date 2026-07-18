3dae9fe2824fd8a19a0bd5cccf3b3d0451e198f6
Arunim Chaudhary <arunim.chaudhary@stackguardian.io>
2026-07-18 17:44:27 +0530

    feat: add invite code field to signup form

--- src/lib/apis/auths/index.ts
+++ src/lib/apis/auths/index.ts
@@ -290,7 +290,8 @@ export const userSignUp = async (
 	name: string,
 	email: string,
 	password: string,
-	profile_image_url: string
+	profile_image_url: string,
+	signup_code: null | string = null
 ) => {
 	let error = null;
 
@@ -304,7 +305,8 @@ export const userSignUp = async (
 			name: name,
 			email: email,
 			password: password,
-			profile_image_url: profile_image_url
+			profile_image_url: profile_image_url,
+			signup_code: signup_code
 		})
 	})
 		.then(async (res) => {
